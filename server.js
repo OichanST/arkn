7b980437977e0077707b34365ffe1e75b30cfd5c
OichanST <56617025+OichanST@users.noreply.github.com>
2020-04-09 16:47:30 +0900

    postgreSQL Server URL change

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 const server = require("http").Server(app);
 const PORT = process.env.PORT || 8080;
 const pg = require("pg");
-const conn = 'postgres://postgres:br9qs6wg@localhost:5432/postgres';
+const conn = 
+//クライアント用"postgres://postgres:br9qs6wg@localhost:5432/postgres";
+"postgres://rosndqtazwwzac:a066047b5ec4f8537219912347a5ef2155dc555c6475210d00593086a0800dde@ec2-184-72-235-80.compute-1.amazonaws.com:5432/d3ku61oage2jn9"
 const pool = new pg.Pool({
 	connectionString:conn
 });
