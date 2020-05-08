90d7ac45da4a1be9f1264d0c388dc8af680091d4
OichanST <56617025+OichanST@users.noreply.github.com>
2020-05-08 16:33:51 +0900

    server change

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const server = require("http").Server(app);
 const PORT = process.env.PORT || 8080;
 const pg = require("pg");
 const conn = 
-"postgres://postgres:br9qs6wg@localhost:5432/postgres";
-//"postgres://rosndqtazwwzac:a066047b5ec4f8537219912347a5ef2155dc555c6475210d00593086a0800dde@ec2-184-72-235-80.compute-1.amazonaws.com:5432/d3ku61oage2jn9"
+//"postgres://postgres:br9qs6wg@localhost:5432/postgres";
+"postgres://rosndqtazwwzac:a066047b5ec4f8537219912347a5ef2155dc555c6475210d00593086a0800dde@ec2-184-72-235-80.compute-1.amazonaws.com:5432/d3ku61oage2jn9"
 const pool = new pg.Pool({
 	connectionString:conn
 /*
